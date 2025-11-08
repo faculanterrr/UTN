877bcd95caff4fcde965dd8a356cb86fed79388c
faculanter <168309403+faculanterrr@users.noreply.github.com>
2025-11-08 16:17:50 -0300

    Update categoryRoute.js

--- TP-backend-final/src/routes/categoryRoute.js
+++ TP-backend-final/src/routes/categoryRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const router = express.Router(); // <--- Verificación
+const router = express.Router(); 
 const categoryController = require('../controllers/categoryController.js');
 const verifyToken = require('../middleware/verifyToken.js')
 
-// Rutas públicas (GET)
+
 router.get('/', categoryController.findAll);
 router.get('/:id', categoryController.findById);
 
-// Rutas protegidas (Requieren JWT)
+
 router.post('/', verifyToken, categoryController.create);
 router.put('/:id', verifyToken, categoryController.update);
 router.delete('/:id', verifyToken, categoryController.delete);
